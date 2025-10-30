a5be8452cade07f8ef232863e577a6af2bb35b8e
Abner <abnerba08@gmail.com>
2025-10-29 23:27:39 -0400

    avance

--- app/src/main/java/com/example/ponchos_rojos/MainActivity.kt
+++ app/src/main/java/com/example/ponchos_rojos/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.ponchos_rojos
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,20 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        // Ajustar paddings según insets (esto ya estaba correcto)
+        val rootView = findViewById<View>(R.id.main)
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+         startActivity(Intent(this@MainActivity, PagarActivity::class.java))
+
+
+
     }
 }
